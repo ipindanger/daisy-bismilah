8e042bab930a8b394e6a9f05b1efa59f060dd9bc
ipindanger <85965834+ipindanger@users.noreply.github.com>
2021-06-26 15:11:00 +0700

    Update afk.py

--- DaisyX/modules/afk.py
+++ DaisyX/modules/afk.py
@@ -37,7 +37,7 @@ def afk(update: Update, context: CallbackContext):
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} is now away!{}".format(fname, notice))
+        update.effective_message.reply_text("{} mancing dulu gengs!{}".format(fname, notice))
     except BadRequest:
         pass
 
@@ -135,12 +135,12 @@ def check_afk(update, context, user_id, fst_name, userc_id):
         if not user.reason:
             if int(userc_id) == int(user_id):
                 return
-            res = "{} is afk".format(fst_name)
+            res = "{} lagi ngebegal ".format(fst_name)
             update.effective_message.reply_text(res)
         else:
             if int(userc_id) == int(user_id):
                 return
-            res = "{} is afk.\nReason: <code>{}</code>".format(
+            res = "{} pergi dulu.\nReason: <code>{}</code>".format(
                 html.escape(fst_name), html.escape(user.reason)
             )
             update.effective_message.reply_text(res, parse_mode="html")
